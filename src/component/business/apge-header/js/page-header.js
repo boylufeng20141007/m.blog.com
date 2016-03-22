d328b8931844be1de82d23e91d6bc20b0bf1a178
lux.lu <lux.lu@vipshop.com>
2016-03-22 20:50:38 +0800

    webpack config

---
+++ src/component/business/apge-header/js/page-header.js
@@ -0,0 +1,18 @@
+/**
+ * @author lux.lu
+ * @date 2016-03-22
+ * 页头
+ */
+
+var React = require('React');
+
+var PageHeader = React.createClass({
+	render: function() {
+		return (
+			<div classID="J-page-header">
+			</div>
+		);
+	}
+});
+
+module.exports = PageHeader;
--- webpack.config.js
+++ webpack.config.js
@@ -5,25 +5,56 @@
  */
  'use strict';
 
+ var path = require('path'),
+ 	 fs = require('fs');
+
  var webpack = require('webpack'),
  	 commonsPlugin = new webpack.optimize.CommonsChunkPlugin('common.js');
 
- module.exports = {
- 	entry: {
- 		entry1: './entry/entry1.js',
- 		entry2: './entry2/entry2.js'
- 	},
+	/*
+	extract-text-webpack-plugin插件，
+	有了它就可以将你的样式提取到单独的css文件里，
+	妈妈再也不用担心样式会被打包到js文件里了。
+	*/
+ var ExtractWebpackPlugin = require('extract-text-webpack-plugin');
+
+	/*
+	html-webpack-plugin插件，重中之重，webpack中生成HTML的插件，
+	具体可以去这里查看https://www.npmjs.com/package/html-webpack-plugin
+	*/
+ var HtmlWebpackPlugin = require('html-webpack-plugin');
+
+ var debug = process.env.NODE_ENV !== 'prod'? true : false;
+
+ var config = {
+ 	entry: {},//配置入口文件，有几个写几个;也可以动态遍历追加
  	output: {
- 		path: __dirname,
- 		filename: '[name].entry.js'
+ 		path: path.join(__dirname, 'dest'),//输出目录的配置，模板、样式、脚本、图片等资源的路径配置都相对于它
+ 		publicPath: '',//模板、样式、脚本、图片等资源对应的server上的路径
+ 		filename: '[name].entry.js',//每个页面对应的主js的生成配置
+ 		chunkFilename: '' //chunk生成的配置
  	},
  	resolve: {
- 		extensions: ['', '.js', '.jsx']
+ 		extensions: ['', '.js', '.jsx', '.jpg', '.png', '.css', '.scss', '.less', 'tpl'],
+ 		alias: {
+ 			base: __dirname + 'src/base',
+ 			component: __dirname + '/src/component',
+ 			page: __dirname + '/src/page'
+ 		}
  	}
  	module: {
  		loaders: [
  			{test: /\.(js)$/, loader: 'jsx-loader?harmony'}
  		]
  	},
- 	plugins: [commonsPlugin]
+ 	plugins: [commonsPlugin],
+ 	devServer: {
+ 		contentBase: 'src/',
+ 		host: 'localhost',
+ 		port: 9090,//默认8080
+ 		inline: true,//可以监控js变化
+ 		hot: true,//热启动
+ 	}
  };
+
+ module.exports = config;
